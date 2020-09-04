b9b8f841709736cbfcb3676226a89c06498aa26f
Markus Scheidgen <markus.scheidgen@gmail.com>
2020-09-04 17:09:56 +0200

    Clean up for next FAIRmat.

--- aptfimparser/__init__.py
+++ aptfimparser/__init__.py
@@ -20,11 +20,8 @@ import numpy as np
 from datetime import datetime
 
 from nomad.parsing.parser import FairdiParser
-from nomad.datamodel.metainfo.general_experimental import (
-    section_experiment as SectionExperiment,
-    section_data as SectionData,
-    section_method as SectionMethod,
-    section_sample as SectionSample)
+from nomad.datamodel.metainfo.common_experimental import (
+    Experiment, Data, Method, Sample, Location, Material)
 
 
 class APTFIMParser(FairdiParser):
@@ -38,54 +35,66 @@ class APTFIMParser(FairdiParser):
         with open(filepath, 'rt') as f:
             data = json.load(f)
 
-        section_experiment = archive.m_create(SectionExperiment)
+        experiment = archive.m_create(Experiment)
+        experiment.raw_metadata = data
 
         # Read general tool environment details
-        section_experiment.experiment_location = data.get('experiment_location')
-        section_experiment.experiment_facility_institution = data.get('experiment_facility_institution')
-        section_experiment.experiment_summary = '%s of %s.' % (data.get('experiment_method').capitalize(), data.get('specimen_description'))
+        location = experiment.m_create(Location)
+        location.address = data.get('experiment_location')
+        location.facility = data.get('experiment_facility_institution')
+        experiment.experiment_summary = '%s of %s.' % (
+            data.get('experiment_method').capitalize(), data.get('specimen_description'))
+
         try:
-            section_experiment.experiment_time = int(datetime.strptime(data.get('experiment_date_global_start'), '%d.%m.%Y %M:%H:%S').timestamp())
+            experiment.experiment_time = datetime.strptime(
+                data.get('experiment_date_global_start'), '%d.%m.%Y %M:%H:%S')
         except ValueError:
             pass
+
         try:
-            section_experiment.experiment_end_time = int(datetime.strptime(data.get('experiment_date_global_end'), '%d.%m.%Y %M:%H:%S').timestamp())
+            experiment.experiment_end_time = datetime.strptime(
+                data.get('experiment_date_global_end'), '%d.%m.%Y %M:%H:%S')
         except ValueError:
             pass
 
         # Read data parameters
-        section_data = section_experiment.m_create(SectionData)
-        section_data.data_repository_name = data.get('data_repository_name')
-        section_data.data_preview_url = data.get('data_repository_url')
+        section_data = experiment.m_create(Data)
+        section_data.repository_name = data.get('data_repository_name')
+        section_data.entry_repository_url = data.get('data_repository_url')
+        section_data.repository_url = '/'.join(data.get('data_repository_url').split('/')[0:3])
         preview_url = data.get('data_preview_url')
         # TODO: This a little hack to correct the preview url and should be removed
         # after urls are corrected
         preview_url = '%s/files/%s' % tuple(preview_url.rsplit('/', 1))
-        section_data.data_preview_url = preview_url
+        section_data.preview_url = preview_url
 
         # Read parameters related to method
-        section_method = section_experiment.m_create(SectionMethod)
-        section_method.experiment_method_name = data.get('experiment_method')
-        section_method.experiment_method_abbreviation = 'APT/FIM'
-        section_method.probing_method = 'electric pulsing'
-        # backend.addValue('experiment_tool_info', data.get('instrument_info')) ###test here the case that input.json keyword is different to output.json
-        # measured_pulse_voltage for instance should be a conditional read
-        # backend.addValue('measured_number_ions_evaporated', data.get('measured_number_ions_evaporated'))
-        # backend.addValue('measured_detector_hit_pos', data.get('measured_detector_hit_pos'))
-        # backend.addValue('measured_detector_hit_mult', data.get('measured_detector_hit_mult'))
-        # backend.addValue('measured_detector_dead_pulses', data.get('measured_detector_dead_pulses'))
-        # backend.addValue('measured_time_of_flight', data.get('measured_time_of_flight'))
-        # backend.addValue('measured_standing_voltage', data.get('measured_standing_voltage'))
-        # backend.addValue('measured_pulse_voltage', data.get('measured_pulse_voltage'))
-        # backend.addValue('experiment_operation_method', data.get('experiment_operation_method'))
-        # backend.addValue('experiment_imaging_method', data.get('experiment_imaging_method'))
+        method = experiment.m_create(Method)
+        method.data_type = 'image'
+        method.method_name = data.get('experiment_method')
+        method.method_abbreviation = 'APT/FIM'
+        method.probing_method = 'electric pulsing'
+        method.instrument_description = data.get('instrument_info')
+
+        method.measured_number_ions_evaporated = data.get('measured_number_ions_evaporated')
+        method.measured_detector_hit_pos = data.get('measured_detector_hit_pos') == 'yes'
+        method.measured_detector_hit_mult = data.get('measured_detector_hit_mult') == 'yes'
+        method.measured_detector_dead_pulses = data.get('measured_detector_dead_pulses') == 'yes'
+        method.measured_time_of_flight = data.get('measured_time_of_flight') == 'yes'
+        method.measured_standing_voltage = data.get('measured_standing_voltage') == 'yes'
+        method.measured_pulse_voltage = data.get('measured_pulse_voltage') == 'yes'
+        method.experiment_operation_method = data.get('experiment_operation_method') == 'yes'
+        method.experiment_imaging_method = data.get('experiment_imaging_method') == 'yes'
 
         # Read parameters related to sample
-        section_sample = section_experiment.m_create(SectionSample)
-        section_sample.sample_description = data.get('specimen_description')
-        section_sample.sample_microstructure = data.get('specimen_microstructure')
-        section_sample.sample_constituents = data.get('specimen_constitution')
+        sample = experiment.m_create(Sample)
+        sample.sample_description = data.get('specimen_description')
+        sample.sample_microstructure = data.get('specimen_microstructure')
+        sample.sample_constituents = data.get('specimen_constitution')
+
+        material = sample.m_create(Material)
         atom_labels = data.get('specimen_chemistry')
         formula = ase.Atoms(atom_labels).get_chemical_formula()
-        section_sample.sample_atom_labels = np.array(atom_labels)
-        section_sample.sample_chemical_formula = formula
+        material.atom_labels = np.array(atom_labels)
+        material.chemical_formula = formula
+        material.chemical_name = formula
--- aptfimparser/metainfo/aptfim.py
+++ aptfimparser/metainfo/aptfim.py
@@ -1,167 +1,48 @@
-import numpy as np            # pylint: disable=unused-import
-import typing                 # pylint: disable=unused-import
-from nomad.metainfo import (  # pylint: disable=unused-import
-    MSection, MCategory, Category, Package, Quantity, Section, SubSection, SectionProxy,
-    Reference
-)
-from nomad.metainfo.legacy import LegacyDefinition
+from nomad.metainfo import Package, Quantity, Section
+from nomad.datamodel.metainfo import common_experimental
 
-from nomad.datamodel.metainfo import general_experimental
-
-m_package = Package(
-    name='aptfim_nomadmetainfo_json',
-    description='None',
-    a_legacy=LegacyDefinition(name='aptfim.nomadmetainfo.json'))
+m_package = Package(name='aptfim')
 
 
-class section_experiment(general_experimental.section_experiment):
+class Method(common_experimental.Method):
 
-    m_def = Section(validate=False, extends_base_section=True, a_legacy=LegacyDefinition(name='section_experiment'))
-
-    none_shape = Quantity(
-        type=int,
-        shape=[],
-        description='''
-        Shape of the None/Null object
-        ''',
-        a_legacy=LegacyDefinition(name='none_shape'))
-
-    experiment_tool_info = Quantity(
-        type=str,
-        shape=[],
-        unit='dimensionless',
-        description='''
-        Name of the equipment, instrument with which the experiment was performed e.g.
-        LEAP5000XS
-        ''',
-        a_legacy=LegacyDefinition(name='experiment_tool_info'))
+    m_def = Section(validate=False, extends_base_section=True)
 
     experiment_operation_method = Quantity(
         type=str,
-        shape=[],
-        unit='dimensionless',
-        description='''
-        Operation mode of the instrument (APT, FIM or combination)
-        ''',
-        a_legacy=LegacyDefinition(name='experiment_operation_method'))
+        description='Operation mode of the instrument (APT, FIM or combination)')
 
     experiment_imaging_method = Quantity(
         type=str,
-        shape=[],
-        unit='dimensionless',
-        description='''
-        Pulsing method to enforce a controlled ion evaporation sequence
-        ''',
-        a_legacy=LegacyDefinition(name='experiment_imaging_method'))
+        description='Pulsing method to enforce a controlled ion evaporation sequence')
 
-    specimen_description = Quantity(
-        type=str,
-        shape=[],
-        unit='dimensionless',
-        description='''
-        Sample description e.g. pure W wire samples trial 2
-        ''',
-        a_legacy=LegacyDefinition(name='specimen_description'))
-
-    number_of_disjoint_elements = Quantity(
+    number_ions_evaporated = Quantity(
         type=int,
-        shape=[],
-        unit='dimensionless',
-        description='''
-        Number of elements (disjoint element names) expected
-        ''',
-        a_legacy=LegacyDefinition(name='number_of_disjoint_elements'))
-
-    specimen_chemistry = Quantity(
-        type=str,
-        shape=['number_of_elements'],
-        unit='dimensionless',
-        description='''
-        List of periodic table names expected contained in dataset
-        ''',
-        a_legacy=LegacyDefinition(name='specimen_chemistry'))
-
-    specimen_microstructure = Quantity(
-        type=str,
-        shape=[],
-        unit='dimensionless',
-        description='''
-        Qualitative type of specimen and microstructure analyzed (e.g. thin films, nano
-        objects, single crystal, polycrystal)
-        ''',
-        a_legacy=LegacyDefinition(name='specimen_microstructure'))
-
-    specimen_constitution = Quantity(
-        type=str,
-        shape=[],
-        unit='dimensionless',
-        description='''
-        Qualitative information how many phases in the specimen
-        ''',
-        a_legacy=LegacyDefinition(name='specimen_constitution'))
-
-    measured_number_ions_evaporated = Quantity(
-        type=np.dtype(np.int32),
-        shape=[],
-        unit='dimensionless',
-        description='''
-        Number of ions successfully evaporated
-        ''',
-        a_legacy=LegacyDefinition(name='measured_number_ions_evaporated'))
+        description='Number of ions successfully evaporated')
 
     measured_detector_hit_pos = Quantity(
-        type=str,
-        shape=[],
-        unit='millimeter ** 2',
-        description='''
-        Detector hit positions x and y
-        ''',
-        a_legacy=LegacyDefinition(name='measured_detector_hit_pos'))
+        type=bool,
+        description='Detector hit positions x and y was measured')
 
     measured_detector_hit_mult = Quantity(
-        type=str,
-        shape=[],
-        unit='dimensionless',
-        description='''
-        Detector hit multiplicity
-        ''',
-        a_legacy=LegacyDefinition(name='measured_detector_hit_mult'))
+        type=bool,
+        description='Detector hit multiplicity was measured')
 
     measured_detector_dead_pulses = Quantity(
-        type=str,
-        shape=[],
-        unit='dimensionless',
-        description='''
-        Detector number of dead pulses
-        ''',
-        a_legacy=LegacyDefinition(name='measured_detector_dead_pulses'))
+        type=bool,
+        description='Detector number of dead pulses was measured')
 
     measured_time_of_flight = Quantity(
-        type=str,
-        shape=[],
-        unit='nanosecond',
-        description='''
-        Raw ion time of flight
-        ''',
-        a_legacy=LegacyDefinition(name='measured_time_of_flight'))
+        type=bool,
+        description='Raw ion time of flight was measured')
 
     measured_standing_voltage = Quantity(
-        type=str,
-        shape=[],
-        unit='volt',
-        description='''
-        Standing voltage
-        ''',
-        a_legacy=LegacyDefinition(name='measured_standing_voltage'))
+        type=bool,
+        description='Standing voltage was measured')
 
     measured_pulse_voltage = Quantity(
-        type=str,
-        shape=[],
-        unit='volt',
-        description='''
-        Pulse voltage
-        ''',
-        a_legacy=LegacyDefinition(name='measured_pulse_voltage'))
+        type=bool,
+        description='Pulse voltage was measured')
 
 
 m_package.__init_metainfo__()
